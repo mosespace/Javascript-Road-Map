aaaab633ec2f3110d38e8a7145b3be3e558f930a
mosespace <kisakyemoses50@gmail.com>
2023-01-30 11:03:59 +0300

    Initial commit added operators arithmetic

---
+++ app.js
@@ -0,0 +1,34 @@
+// console.log('Hello World');
+
+/*
+OPERATORS
+
+1)Assignment Operator
+2)arithmetic operators
+
+
+*/
+
+const age = 30;
+const firstName = 'Moses';
+const secondName = 'Kisakye';
+const isMarried = false;
+const job = 'frontend developer';
+const x = 40;
+const y = 30;
+
+
+// addition
+console.log(x + y);
+
+// subtraction
+console.log(x - y);
+
+// multiplication
+console.log(x * y);
+
+// exponential
+console.log(x ** 2); // is equal to x^2 =x*x
+
+// Modulus => %
+console.log(y % x); //what is the reminder of dividing 20 to 10
