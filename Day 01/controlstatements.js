97d08a3fcee67b4dc6e9e53d638ef8009bbca09b
mosespace <kisakyemoses50@gmail.com>
2023-02-02 20:16:39 +0300

    modified

--- Day 01/controlstatements.js
+++ Day 01/controlstatements.js
@@ -1,3 +1,26 @@
 // If statements
 
+const x = 100;
+const y = 90;
+const j = x / 2;
+
+if (x > y) {
+    console.log("X is greater than Y");
+} else if (x % 2) {
+    console.log("X is divisible by two");
+} else if (x < y) {
+    console.log("X is less than Y");
+} else if (x / 2) {
+    console.log("X is divided by 2 and the answer is " + j);
+} else {
+    console.log("X is nothing");
+}
+/*
+Here X will take over line 7 because it's the first true statemnet
+*/
+
+
+
+
+
 // switch statements
