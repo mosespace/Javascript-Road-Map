390416c531cad0deef004d4aa81d880ab10dd017
mosespace <kisakyemoses50@gmail.com>
2023-02-02 23:16:24 +0300

    update2

--- Day 01/functions.js
+++ Day 01/functions.js
@@ -13,7 +13,7 @@ const userMessage = prompt("Write Your Full Names Here;");
 sayHelloWorld(userMessage);*/
 
 // Create a program that takes in two arguments / two numbers and then it returns there sum.
-
+/*
 function addNumbers(numberOne, numberTwo) {
     const sum = Number(numberOne) + Number(numberTwo);
     console.log(`The sum of both ${numberOne} and ${numberTwo} is ${sum}`);
@@ -22,7 +22,26 @@ function addNumbers(numberOne, numberTwo) {
 // call or invoke of the numbers
 const value1 = prompt('Enter Value 1');
 const value2 = prompt('Enter Value 2');
-addNumbers(value1, value2);
+addNumbers(value1, value2);*/
+
+
+/* //Question
+Qtn. 1 |Create a program that takes in four arguments and adds them first and later then divide the total by two*/
+
+function divideNumbers(number1, number2, number3, number4) {
+    const sum = Number(number1) + Number(number2) + Number(number3) + Number(number4);
+    const divide = sum / 2;
+    console.log(`The sum of all the four numbers is ${sum} and there division is ${divide}`);
+}
+
+// call or invoke of the numbers
+const add1 = prompt('Enter Value 1');
+const add2 = prompt('Enter Value 2');
+const add3 = prompt('Enter Value 3');
+const add4 = prompt('Enter Value 4');
+
+divideNumbers(add1, add2, add3, add4);
+
 
 /*//  Write a program that calculates area of a triangle.
 // Area is = 1/2 * B * H
