c157bde30c9276ca12b591f92ce91d5befeefacc
mosespace <kisakyemoses50@gmail.com>
2023-02-03 12:27:52 +0300

    Modified exersises.js

---
+++ Lesson 1/3/exersises.js
@@ -0,0 +1,9 @@
+// Use the correct assignment operator that will result in x being 15 (same as x = x + y).
+x = 10;
+y = 5;
+x /*add missing*/ y;
+
+// Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+x = 10;
+y = 5;
+x /*add missing*/ y;
