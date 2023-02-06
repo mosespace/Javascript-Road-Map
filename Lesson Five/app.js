b4736ed4682f06d9148228c78e2734fc0cceeea3
mosespace <kisakyemoses50@gmail.com>
2023-02-06 05:45:53 +0300

    stringSubstring

--- Lesson Five/app.js
+++ Lesson Five/app.js
@@ -18,3 +18,10 @@ const str = "Hello World";
 console.log(str.slice(0, 5));
 console.log(str.slice(5)); // Extracts values from index five to the positive side.
 console.log(str.slice(-5)); // Extracts values from the positive side to the negative side.
+
+/*----------------Example Three-----------------*/
+// 3) substring Split => It is the sasme with the String method but generally doesnot accept the negative index ;
+const lang = "Javascript";
+console.log(lang.slice(0, 5));
+console.log(lang.slice(5)); // Extracts values from index five to the positive side.
+console.log(lang.slice(5, 0)); // Extracts values in reverse order, it re arrages the smallest number to come after the biggest number has passed.
