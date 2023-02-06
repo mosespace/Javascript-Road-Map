5b5d3d684c1b7a371ca4c7847bbe000bb8419248
mosespace <kisakyemoses50@gmail.com>
2023-02-06 05:29:39 +0300

    stringLenght

--- Lesson Five/app.js
+++ Lesson Five/app.js
@@ -1,1 +1,13 @@
 console.log('Hello World')
+    // STRING METHODS
+    // 1) Length => number of characters including space:
+
+/*----------------Example One-----------------*/
+let alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+let numberOfAlphabets = alphabets.length;
+
+if (numberOfAlphabets < 26) {
+    console.log('The "Alphabets" are missing');
+} else {
+    console.log('The "Alphabets" are matching');
+}
