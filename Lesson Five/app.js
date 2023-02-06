6d8022e4cf1417cbd1987916e3bc14ffa2590d3d
mosespace <kisakyemoses50@gmail.com>
2023-02-06 05:38:47 +0300

    stringSlice

--- Lesson Five/app.js
+++ Lesson Five/app.js
@@ -11,3 +11,10 @@ if (numberOfAlphabets < 26) {
 } else {
     console.log('The "Alphabets" are matching');
 }
+
+/*----------------Example Two-----------------*/
+// 2) String Slice => count's from the start to end but excluding the end: (0,6)--Index 6 won't be seen in the output if the string is printed out;
+const str = "Hello World";
+console.log(str.slice(0, 5));
+console.log(str.slice(5)); // Extracts values from index five to the positive side.
+console.log(str.slice(-5)); // Extracts values from the positive side to the negative side.
