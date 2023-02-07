10918c89054d34cc2abb693a73f1266f94fb3a88
mosespace <kisakyemoses50@gmail.com>
2023-02-07 17:26:46 +0300

    storing arrays

--- Lesson Six/app.js
+++ Lesson Six/app.js
@@ -7,7 +7,7 @@ if the sum of both numbers is less than 100. Otherwise
 return false.*/
 // sumOfNumbers(10, 20)➞ true
 //10+20=40<100
-
+/*
 const sumOf = (x, y) => {
     const sum = x + y;
     if (sum < 100) {
@@ -19,7 +19,7 @@ const sumOf = (x, y) => {
 };
 
 const finalSum = sumOf(parseFloat(prompt('Enter Number One')), parseFloat(prompt('Enter Number Two')));
-console.log(finalSum);
+console.log(finalSum);*/
 
 
 /*const x = prompt('Enter Number One');
@@ -30,3 +30,23 @@ if (x + y < 100) {
 } else if (x + y >= 100) {
     console.log('False');
 };*/
+
+
+
+
+
+
+
+
+
+/*<----------------ARRAYS---------------->*/
+/*
+JavaScript Arrays:
+They are used to store a collection of data. It can be of a different type of any data type!
+How do we know that it is an array: The data is represented in by the following brackets"[]" All elements in an array ara comma separated
+
+Example:
+*/
+
+const myArrow = ['Moses', 'Daniel', 'Tosh', 'Kisakye Moses', 'Fave Artisit']
+console.log(myArrow)
