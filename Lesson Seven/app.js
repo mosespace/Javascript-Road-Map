cbecedff0d8928eb9316bd4b57a5abde8046268d
mosespace <kisakyemoses50@gmail.com>
2023-02-08 14:16:34 +0300

    for_loop_one

--- Lesson Seven/app.js
+++ Lesson Seven/app.js
@@ -1,1 +1,17 @@
-console.log("Hello It's Working");
+// JavaScript Loops!
+
+/*
+A loop in javascript is used to execute a block of code with a specified number of elements.
+There are many examples of loops but first we are going to look at the for loop.
+In this method, we do it by the following
+
+for (initialization; condition; increment;){
+    // then here we pass the code and print it out in the console
+}
+*/
+
+//  Let's assume we want to do aloop on plates using the for loop: we do this
+
+for (let plates = 1; plates <= 10; plates++) {
+    console.log(`Plate Number ${plates}, You have recieved a 🎂 `)
+}
