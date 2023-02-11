b2599af281113973ae4d31e3908dcf4bd2e1e59e
mosespace <kisakyemoses50@gmail.com>
2023-02-11 23:50:31 +0300

    creating

--- Lesson Nine/app.js
+++ Lesson Nine/app.js
@@ -4,3 +4,5 @@ DOM Document Object Module is a web API that allows developers to use programmin
 
 It makes static websites to Dynamic Websites
 */
+
+// You can use the createElement method of an object to create an element withot html or css!!!
