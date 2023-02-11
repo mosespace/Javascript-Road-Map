8a05c469554788377db9255f6e0acf57a962301f
mosespace <kisakyemoses50@gmail.com>
2023-02-11 23:36:41 +0300

    DOM one

---
+++ Lesson Nine/app.js
@@ -0,0 +1,6 @@
+/*
+DOM Document Object Module is a web API that allows developers to use programming logic to make changes to there HTML code and css!
+
+
+It makes static websites to Dynamic Websites
+*/
