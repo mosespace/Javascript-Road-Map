941fbb1bfcff7a464a6191196bef1be6502cf27a
mosespace <kisakyemoses50@gmail.com>
2023-02-13 22:47:40 +0300

    added example

--- Lesson Ten/app.js
+++ Lesson Ten/app.js
@@ -1,1 +1,16 @@
 // Manuplation Of DOM
+
+// Everything comes to be a camel case when modifying elements attributes! ie;
+/*
+In Css we rwrite font-size = ' '
+but in javascript we turn it to fontSize = ' '
+
+The same applies to others like Background-color in css
+but in javascript = backgroundColor = ' 's
+*/
+/* ------------------------Example One---------------------*/
+const headings = document.createElement('h1');
+
+headings.innerHTML = 'Advanced Dom';
+
+document.body.appendChild(headings);
